3261edc915ca8ba762a70dfcd37008a8ebea8c4a
TiyaSeesStars <adwitiya.pal1@gmail.com>
2025-06-12 11:40:16 -0400

    Update script.js
    added log and diary

--- script.js
+++ script.js
@@ -34,3 +34,15 @@ function updateDisplay() {
   lightDisplay.textContent = `Light: ${light.toFixed(1)}`;
   fingersOwned.textContent = `Fingers: ${fingers}`;
 }
+
+// the log and diary
+function openLog(tabName) {
+  const tabs = document.querySelectorAll(".log-content");
+  const buttons = document.querySelectorAll(".tab-btn");
+
+  tabs.forEach(tab => tab.classList.remove("active"));
+  buttons.forEach(btn => btn.classList.remove("active"));
+
+  document.getElementById(tabName).classList.add("active");
+  event.target.classList.add("active");
+}
